5019c83eea835d1ea2d0d25f2012de11bf599b85
Isaac Gamboa <isok_gamboa1999@hotmail.com>
2023-11-13 04:19:55 -0500

    ADD:se inicializa api rest, con estructura basica de proyecto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+import usersRoutes from './routes/users.routes.js'
+import indexRoutes from './routes/index.routes.js'
+
+const app = express()
+
+app.use(indexRoutes)
+app.use(usersRoutes)
+
+app.listen(3000)
+console.log('server is running in port 3000')
+
