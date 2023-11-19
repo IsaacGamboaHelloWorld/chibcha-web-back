6880938a22991ffd8ba78bd8b3335e7319d7c5be
Isaac Gamboa <isok_gamboa1999@hotmail.com>
2023-11-18 21:05:38 -0500

    cambios en users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,16 @@
+import {pool} from './../db.js'
+
 export const getUsers = (req , res) => res.send('obteniendo usuarios')
 
-export const createUser = (req , res) => res.send('creando usuarios')
+export const createUser = async (req , res) =>{
+    const {name,last_name,email,password,role_id} = req.body
+    await pool.query(
+        'INSERT INTO users (role_id, email, name, last_name, password) VALUES (?, ?, ?, ?, ?)',
+        [role_id,email,name,last_name,password]
+        )
+
+    res.send('funciono')
+}
 
 export const updateUsers = (req , res) => res.send('actualizando empleados')
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import usersRoutes from './routes/users.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
 const app = express()
+app.use(express.json())
 
 app.use(indexRoutes)
 app.use(usersRoutes)
